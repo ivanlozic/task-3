6143727660757d023ee2b203c8ba41bf349fab4b
Ivan Lozic <ivanlozic995@gmail.com>
2024-11-15 12:31:18 +0100

    init

---
+++ script.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", () => {
+  new Swiper(".swiper", {
+    loop: true,
+    speed: 1000,
+    direction: "horizontal",
+    navigation: {
+      nextEl: ".swiper-next",
+      prevEl: ".swiper-prev",
+    },
+    breakpoints: {
+      375: {
+        slidesPerView: 1.05,
+        spaceBetween: 10,
+      },
+      1440: {
+        slidesPerView: 2.8,
+        spaceBetween: 20,
+      },
+    },
+  });
+});
+
+// document.getElementById("toggleSwiper").addEventListener("click", () => {
+//     if (swiperInstance && swiperInstance.destroyed === false) {
+//       swiperInstance.destroy();
+//       swiperInstance = null;
+//       console.log("Swiper Destroyed");
+//     } else {
+//       initializeSwiper();
+//       console.log("Swiper Initialized");
+//     }
+//   });
